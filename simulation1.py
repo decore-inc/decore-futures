df97c23d7243d02c16c11ca5fd8bd150d03d7b55
Bob Lee <bob@venture-g.com>
2019-11-28 13:10:29 +0800

    fixed uniswap calculation

--- simulation1.py
+++ simulation1.py
@@ -11,8 +11,8 @@ result = []
 buy = 0
 sell = 0
 rolled_pnl = 0
-total_sell = 0
-total_buy = 0
+rolled_sell = 0
+rolled_buy = 0
 rolled_pnl = 0
 for chunk in pd.read_csv(filename, chunksize=chunksize):
     df = pd.DataFrame(chunk, columns=['delta_size', 'timestamp'])
@@ -20,14 +20,14 @@ for chunk in pd.read_csv(filename, chunksize=chunksize):
         size = delta[0]
         timestamp = delta[1]
         ft_amount += size
-        ft_dai_amount -= size * price
         price = ft_dai_amount / ft_amount
+        ft_dai_amount -= size * price
         pnl = size * price
         rolled_pnl += pnl
         sell = pnl if size > 0 else 0
         buy = -pnl if size < 0 else 0
-        total_sell += sell * price
-        total_buy += buy * price
+        rolled_sell += sell
+        rolled_buy += buy
         result.append([
             size,
             ft_amount,
@@ -36,15 +36,20 @@ for chunk in pd.read_csv(filename, chunksize=chunksize):
             buy,
             sell,
             timestamp,
-            rolled_pnl
+            rolled_pnl,
+            rolled_sell,
+            rolled_buy,
         ])
-print(total_sell)
+total_buy = rolled_buy
+total_sell = rolled_sell
+total_pnl = rolled_buy - rolled_sell
+total_pnl_rate = total_pnl / (total_buy + total_sell)
 print(total_buy)
-print(total_buy - total_sell)
-print(total_buy + total_sell)
-print((total_buy - total_sell)/(total_sell + total_buy))
+print(total_sell)
+print(total_pnl)
+print(total_pnl_rate)
 data = np.array(result)
 df2 = pd.DataFrame(
-    {'size': data[:, 0], 'ft_amount': data[:, 1], 'price': data[:, 2], 'pnl': data[:, 3], 'buy': data[:, 4], 'sell': data[:, 5], 'timestamp': data[:, 6], 'rolled_pnl': data[:, 7]},
-    columns=['size', 'ft_amount', 'price', 'pnl', 'buy', 'sell', 'timestamp', 'rolled_pnl'])
-df2.to_csv('results/prices-10**8-pnl.csv', index=None, header=True)
+    {'size': data[:, 0], 'ft_amount': data[:, 1], 'price': data[:, 2], 'pnl': data[:, 3], 'buy': data[:, 4], 'sell': data[:, 5], 'timestamp': data[:, 6], 'rolled_pnl': data[:, 7], 'rolled_sell': data[:, 8], 'rolled_buy': data[:, 9]},
+    columns=['size', 'ft_amount', 'price', 'pnl', 'buy', 'sell', 'timestamp', 'rolled_pnl', 'rolled_sell', 'rolled_buy'])
+df2.to_csv('results/sim1-prices-pnl.csv', index=None, header=True)
--- simulation2.py
+++ simulation2.py
@@ -13,25 +13,25 @@ buy = 0
 sell = 0
 rolled_size = 0
 rolled_pnl = 0
-total_sell = 0
-total_buy = 0
+rolled_sell = 0
+rolled_buy = 0
 for chunk in pd.read_csv(filename, chunksize=chunksize):
     df = pd.DataFrame(chunk, columns=['delta_size', 'timestamp'])
     for delta in df.values:
         size = delta[0]
         timestamp = delta[1]
         ft_amount += size
-        ft_dai_amount -= size * price
         rolled_size += size
         k = rolled_size / size_max
-        rate = (3 / 4 * k) + (5 / 4)
+        rate = (3 / 4 * (-k)) + (5 / 4)
         price = rate * ft_dai_amount / ft_amount
+        ft_dai_amount -= size * price
         pnl = size * price
         rolled_pnl += pnl
         sell = pnl if size > 0 else 0
         buy = -pnl if size < 0 else 0
-        total_sell += sell * price
-        total_buy += buy * price
+        rolled_sell += sell
+        rolled_buy += buy
         result.append([
             size,
             ft_amount,
@@ -42,15 +42,20 @@ for chunk in pd.read_csv(filename, chunksize=chunksize):
             timestamp,
             k,
             rate,
-            rolled_pnl
+            rolled_pnl,
+            rolled_sell,
+            rolled_buy
         ])
-print(total_sell)
+total_buy = rolled_buy
+total_sell = rolled_sell
+total_pnl = rolled_buy - rolled_sell
+total_pnl_rate = total_pnl / (total_buy + total_sell)
 print(total_buy)
-print(total_buy - total_sell)
-print(total_buy + total_sell)
-print((total_buy - total_sell)/(total_sell + total_buy))
+print(total_sell)
+print(total_pnl)
+print(total_pnl_rate)
 data = np.array(result)
 df2 = pd.DataFrame(
-    {'size': data[:, 0], 'ft_amount': data[:, 1], 'price': data[:, 2], 'pnl': data[:, 3], 'buy': data[:, 4], 'sell': data[:, 5], 'timestamp': data[:, 6], 'k': data[:, 7], 'rate': data[:, 8], 'rolled_pnl': data[:, 9]},
-    columns=['size', 'ft_amount', 'price', 'pnl', 'buy', 'sell', 'timestamp', 'k', 'rate', 'rolled_pnl'])
-df2.to_csv('results/sim2-prices-pnl.csv', index=None, header=True)
+    {'size': data[:, 0], 'ft_amount': data[:, 1], 'price': data[:, 2], 'pnl': data[:, 3], 'buy': data[:, 4], 'sell': data[:, 5], 'timestamp': data[:, 6], 'k': data[:, 7], 'rate': data[:, 8], 'rolled_pnl': data[:, 9], 'rolled_sell': data[:, 10], 'rolled_buy': data[:, 11]},
+    columns=['size', 'ft_amount', 'price', 'pnl', 'buy', 'sell', 'timestamp', 'k', 'rate', 'rolled_pnl', 'rolled_sell', 'rolled_buy'])
+df2.to_csv('results/sim2-nagative-k.csv', index=None, header=True)
