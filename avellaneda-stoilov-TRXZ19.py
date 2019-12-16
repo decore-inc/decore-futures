fix formula

avellaneda-stoilov-TRXZ19.py
@@ -3,13 +3,12 @@ import numpy as np
 
 filename = 'sources/TRXZ19_size_with_sign.csv'
 chunksize = 10 ** 8
-base_token_in_pool = 10.0 ** 8
-TWAP = 60
-p_token_in_pool = base_token_in_pool / TWAP
+init_base_token_in_pool = 10.0 ** 7
+base_token_in_pool = init_base_token_in_pool
+TWAP_price = 2.41 * 10 ** -6
+p_token_in_pool = base_token_in_pool / TWAP_price
 supply_invariant = base_token_in_pool * p_token_in_pool
-minted_long_p_token = p_token_in_pool / 2
-minted_short_p_token = p_token_in_pool / 2
-q = minted_long_p_token - minted_short_p_token
+q = 0
 upper_delta = 1 * 0.01
 G = 10 * 0.01
 
@@ -19,36 +18,40 @@ rolled_buy = 0
 rolled_sell = 0
 for chunk in pd.read_csv(filename, chunksize=chunksize):
     df = pd.DataFrame(chunk, columns=['size', 'timestamp'])
+    count = 0
     for delta in df.values:
-        size = delta[0]  # Negative if Buy
+        if count > 100: break
+        p_token_to_buyer = delta[0]  # Negative if Buy
         timestamp = delta[1]
 
-        base_token_in_pool += size
-        p_token_in_pool = base_token_in_pool / TWAP
-        p_token_price = size / (p_token_in_pool - (supply_invariant / base_token_in_pool))
-        supply_invariant = base_token_in_pool * p_token_in_pool
-        if size > 0:
-            minted_long_p_token = (minted_long_p_token - size) / TWAP
-        else:
-            minted_short_p_token = (minted_short_p_token + size) / TWAP
-        q = minted_long_p_token - minted_short_p_token
+        p_token_in_pool += p_token_to_buyer
+        base_token_price = p_token_to_buyer / (base_token_in_pool - (supply_invariant / p_token_in_pool))
+        p_token_price = 1 / base_token_price
+        base_token_from_buyer = - p_token_to_buyer * p_token_price
+        base_token_in_pool += base_token_from_buyer
+        q -= p_token_to_buyer
         _q = (q * p_token_price - base_token_in_pool) / (q * p_token_price + base_token_in_pool)
         long_p_token_price = p_token_price * (1.0 - _q * G + upper_delta)
         short_p_token_price = p_token_price * (1.0 - _q * G - upper_delta)
 
-        pnl = size * p_token_price
+        isBuy = p_token_to_buyer < 0
+        if isBuy:
+            pnl = p_token_to_buyer * long_p_token_price
+        else:
+            pnl = p_token_to_buyer * short_p_token_price
         rolled_pnl += pnl
-        buy = -pnl if size < 0 else 0
-        sell = pnl if size > 0 else 0
+        buy = -pnl if isBuy else 0
+        sell = pnl if not isBuy else 0
         rolled_buy += buy
         rolled_sell += sell
 
         result.append([
-            size,
-            base_token_in_pool,
+            p_token_to_buyer,
+            p_token_in_pool,
             p_token_price,
-            minted_long_p_token,
-            minted_short_p_token,
+            base_token_from_buyer,
+            base_token_in_pool,
+            base_token_price,
             long_p_token_price,
             short_p_token_price,
             pnl,
@@ -59,6 +62,7 @@ for chunk in pd.read_csv(filename, chunksize=chunksize):
             rolled_buy,
             rolled_sell
         ])
+        count += 1
 total_buy = rolled_buy
 total_sell = rolled_sell
 total_pnl = rolled_buy - rolled_sell
@@ -70,25 +74,27 @@ print(total_pnl_rate)
 data = np.array(result)
 df2 = pd.DataFrame(
     {
-        'size': data[:, 0],
-        'base_token_in_pool': data[:, 1],
+        'p_token_to_buyer': data[:, 0],
+        'p_token_in_pool': data[:, 1],
         'p_token_price': data[:, 2],
-        'minted_long_p_token': data[:, 3],
-        'minted_short_p_token': data[:, 4],
-        'long_p_token_price': data[:, 5],
-        'short_p_token_price': data[:, 6],
-        'pnl': data[:, 7],
-        'buy': data[:, 8],
-        'sell': data[:, 9],
-        'timestamp': data[:, 10],
-        'rolled_pnl': data[:, 11],
-        'rolled_buy': data[:, 12],
-        'rolled_sell': data[:, 13]},
-    columns=['size',
-             'base_token_in_pool',
+        'base_token_from_buyer': data[:, 3],
+        'base_token_in_pool': data[:, 4],
+        'base_token_price': data[:, 5],
+        'long_p_token_price': data[:, 6],
+        'short_p_token_price': data[:, 7],
+        'pnl': data[:, 8],
+        'buy': data[:, 9],
+        'sell': data[:, 10],
+        'timestamp': data[:, 11],
+        'rolled_pnl': data[:, 12],
+        'rolled_buy': data[:, 13],
+        'rolled_sell': data[:, 14]},
+    columns=['p_token_to_buyer',
+             'p_token_in_pool',
              'p_token_price',
-             'minted_long_p_token',
-             'minted_short_p_token',
+             'base_token_from_buyer',
+             'base_token_in_pool',
+             'base_token_price',
              'long_p_token_price',
              'short_p_token_price',
              'pnl',
@@ -98,4 +104,6 @@ df2 = pd.DataFrame(
              'rolled_pnl',
              'rolled_buy',
              'rolled_sell'])
-df2.to_csv('results/avellaneda-stoilov-TRXZ19.csv', index=None, header=True)
+init_base_token_in_pool = '{:.2e}'.format(init_base_token_in_pool)
+TWAP = '{:.2e}'.format(TWAP_price)
+df2.to_csv(f'results/TRXZ19-base_token_in_pool={init_base_token_in_pool}-TWAP={TWAP}-delta={upper_delta}-G={G}.csv', index=None, header=True)