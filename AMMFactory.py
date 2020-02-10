9333d7b835cf140072bae25864cc818a49b10d24
Yenwen Feng <tempofeng@gmail.com>
2020-02-10 15:58:56 +0800

    refactoring

--- AMMFactory.py
+++ AMMFactory.py
@@ -5,7 +5,8 @@ import pandas as pd
 
 class AMMFactory:
 
-    def create_AMM(self, source_symbol, init_base_token_in_pool, twap_price, delta, g, fee_rate, limit=None):
+    def create_AMM(self, source_symbol, init_base_token_in_pool, twap_price, delta, g, fee_rate, limit=None, sample=1,
+                   export_all_trades=False):
         amm = AMM(init_base_token_in_pool, twap_price, delta, g, fee_rate)
         count = 0
         start_time = datetime.now()
@@ -14,9 +15,11 @@ class AMMFactory:
         for chunk in chunks:
             df = pd.DataFrame(chunk, columns=['size', 'side', 'price', 'timestamp'])
             if len(df.values) == 0: raise ValueError("Trade length is Zero, there is no output csv.")
-            for value in df.sort_values(by=['timestamp']).values:
+            for idx, value in enumerate(df.sort_values(by=['timestamp']).values):
                 if (limit is not None) and (limit > 0) and (count >= limit):
                     break
+                if idx % sample != 0:
+                    continue
                 side = value[1]
                 if side is None:
                     print(f'side is null: {value}')
@@ -24,23 +27,21 @@ class AMMFactory:
                 p_token_to_buyer = -value[0] if side == 'Buy' else value[0]  # Negative if Buy
                 target_price = value[2]
                 timestamp = value[3]
-
                 amm.make_trade(p_token_to_buyer, target_price, timestamp)
                 amm.market_maker(target_price, timestamp)
-
                 count += 1
-        data_frame = {}
-        keys = amm.trades[0].__dict__.keys()
-        for key in keys:
-            data_frame[key] = [trade.__dict__[key] for trade in amm.trades]
-        df2 = pd.DataFrame(
-            data_frame,
-            columns=keys)
-        init_base_token_in_pool = '{:.2e}'.format(init_base_token_in_pool)
-        output_filename = f'{source_symbol}-base_token_in_pool={init_base_token_in_pool}-TWAP={twap_price}-delta={delta}-G={g}-fee={fee_rate}'
-        df2.to_csv(f'results/{source_symbol}/{output_filename}.csv', index=None, header=True)
-        print(f'Output file: {output_filename}.csv, Process duration time: {datetime.now() - start_time}')
-        print(amm)
+
+        if export_all_trades:
+            data_frame = {}
+            keys = amm.trades[0].__dict__.keys()
+            for key in keys:
+                data_frame[key] = [trade.__dict__[key] for trade in amm.trades]
+            df2 = pd.DataFrame(
+                data_frame,
+                columns=keys)
+            init_base_token_in_pool = '{:.2e}'.format(init_base_token_in_pool)
+            output_filename = f'{source_symbol}-base_token_in_pool={init_base_token_in_pool}-TWAP={twap_price}-delta={delta}-G={g}-fee={fee_rate}'
+            df2.to_csv(f'results/{source_symbol}/{output_filename}.csv', index=None, header=True)
+            print(f'Output file: {output_filename}.csv, Process duration time: {datetime.now() - start_time}')
+            print(amm)
         return amm
-
-
--- start.py
+++ start.py
@@ -1,38 +1,48 @@
+import os
+
 import pandas as pd
 from AMMFactory import AMMFactory
 
 
 def run_simulations():
-    source_symbol = 'XBTH19'
-    twap_price = 6850
-    fee_rate = 0.05 * 10 ** -2 # 0.05 * 10 ** -2
-    limit = None # None if unlimited
+    for source_symbol in ['ETHM18']:
+        run_simulation(source_symbol)
 
-    init_base_token_in_pool_list = [10 ** 7, 10 ** 8, 10 ** 9]
-    delta_list = [0.001, 0.002]
-    g_list = [0.01, 0.001]
+
+def run_simulation(source_symbol):
+    twap_price = 0.04264
+    fee_rate = 0  # 0.05 * 10 ** -2
+    limit = None  # None if unlimited
+
+    init_base_token_in_pool_list = [10 ** 7]
+    delta_list = [0]
+    g_list = [0]
+    samples = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
     amm_factory = AMMFactory()
     data = []
     for init_base_token_in_pool in init_base_token_in_pool_list:
         for delta in delta_list:
             for g in g_list:
-                print(f'source_symbol: {source_symbol}')
-                print(f'init_base_token_in_pool: {init_base_token_in_pool}')
-                print(f'twap_price: {twap_price}')
-                print(f'delta: {delta}')
-                print(f'G: {g}')
-                print(f'fee_rate: {fee_rate}')
-                print(f'limit: {limit}')
-                amm = amm_factory.create_AMM(source_symbol, init_base_token_in_pool, twap_price, delta, g, fee_rate, limit)
-                amm.init_base_token_in_pool = init_base_token_in_pool
-                amm.calculate_max_rolled_pnl()
-                # noinspection PyTypeChecker
-                amm.trades = len(amm.trades)
-                data.append(amm.__dict__)
+                for sample in samples:
+                    print(f'source_symbol: {source_symbol}')
+                    print(f'init_base_token_in_pool: {init_base_token_in_pool}')
+                    print(f'twap_price: {twap_price}')
+                    print(f'delta: {delta}')
+                    print(f'G: {g}')
+                    print(f'fee_rate: {fee_rate}')
+                    print(f'limit: {limit}')
+                    amm = amm_factory.create_AMM(source_symbol, init_base_token_in_pool, twap_price, delta, g, fee_rate,
+                                                 limit, sample)
+                    amm.init_base_token_in_pool = init_base_token_in_pool
+                    amm.calculate_max_rolled_pnl()
+                    # noinspection PyTypeChecker
+                    amm.trades = len(amm.trades)
+                    data.append(amm.__dict__)
     df = pd.DataFrame(data)
-    filename = f'{source_symbol}/base_token_in_pool={init_base_token_in_pool_list}-delta={delta_list}-g={g_list}.json'
-    df.to_json(f'results/{filename}', orient='records')
+    filename = f'{source_symbol}/delta={delta_list}-g={g_list}.csv'
+    df.to_csv(f'results/{filename}', index=None, header=True)
     print(f'Output file: {filename}')
+
 
 run_simulations()
